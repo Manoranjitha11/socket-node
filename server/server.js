43d249aa2543b032815b17038f4de704bdcdb5af
Manoranjitha <manoranjitha.s@tringapps.com>
2018-03-19 17:13:12 +0530

    Socket with custom events

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
 var app = express();
-
+var socketIO = require('socket.io');
+var server = http.createServer(app);
+var io = socketIO(server);
 const publicpath = path.join(__dirname, '../public');
-console.log(publicpath);
-
+//console.log(publicpath);
+io.on('connection', (socket) => {
+  console.log("New user connected");
+  socket.emit('newMail', {
+    name: 'Manoranjitha',
+    address : 'salem'
+  })
+  socket.on('createMail', (data) => {
+console.log('From clientVersion', data);
+  })
+  socket.on('disconnect' , () => {
+    console.log("Disconnected from client");
+  })
+})
 var port = process.env.PORT || 3000;
 app.use(express.static(publicpath));
-app.listen(port);
+server.listen(port);
